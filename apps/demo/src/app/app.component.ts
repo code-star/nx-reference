fa364900ebf707a87ef12832402b7e90d94352cc
M.D. van Es <es@mdworld.nl>
2021-07-02 12:05:44 +0200

    Show rates

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -8,18 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 /**
  * This is test documentation, part 2 TODO
  */
 export class AppComponent {
   title = 'demo';
   loading = false;
+  rates = [];
 
   getRate(): void {
-    console.log('yo');
     this.loading = true;
     setTimeout(() => {
       this.loading = false;
+      this.rates.push(Math.random())
     }, 1000);
   }
 }
