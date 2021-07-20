b4f167ca42a848472da7ba09d46451f2474b7cc9
M.D. van Es <es@mdworld.nl>
2021-07-20 15:26:34 +0200

    Add message implementation

--- libs/shared/services/src/lib/message.service.ts
+++ libs/shared/services/src/lib/message.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessageService {
+  messages: string[] = [];
 
-  constructor() { }
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
 }
