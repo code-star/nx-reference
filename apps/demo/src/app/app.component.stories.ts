9bc12ae3a9e24937bad945f29c7b239e4a29bc0a
M.D. van Es <es@mdworld.nl>
2021-07-20 15:01:47 +0200

    Show packagejson version

--- apps/demo/src/app/app.component.stories.ts
+++ apps/demo/src/app/app.component.stories.ts
@@ -1,9 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { UiModule } from '@star/ui';
 import { AppComponent } from './app.component';
+import { version } from '../../../../package.json';
 
 export default {
-  title: 'AppComponent',
+  title: `AppComponent v${version}`,
   component: AppComponent,
   parameters: {
     layout: 'fullscreen',
