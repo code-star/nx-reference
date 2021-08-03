fd54eed2635cb1b9fa62d8ea93d78f29926cb986
M.D. van Es <es@mdworld.nl>
2021-08-03 09:45:59 +0200

    Remove unused label

--- libs/ui/src/lib/loading-button/loading-button.component.ts
+++ libs/ui/src/lib/loading-button/loading-button.component.ts
@@ -9,27 +9,8 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./loading-button.component.scss'],
 })
 export class LoadingButtonComponent {
-  private _label_orig: string;
-  private _loading: boolean;
-
   /**
    * When loading is true, the button is disabled and a loading animation is displayed.
    */
-  @Input() set loading(value: boolean) {
-    this._loading = value;
-
-    if (this.label !== 'Loading...') {
-      this._label_orig = this.label;
-    }
-
-    this.label = value ? 'Loading...' : this._label_orig;
-  }
-  get loading(): boolean {
-    return this._loading;
-  }
-
-  /**
-   * The label to display on the button
-   */
-  @Input() label: string;
+  @Input() loading: boolean;
 }
