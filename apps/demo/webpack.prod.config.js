e8288b3625c73b7a9b3882cd8ae12b3b9a44e510
M.D. van Es <es@mdworld.nl>
2021-11-06 14:58:40 +0100

    Dynamically set remote url

--- apps/demo/webpack.prod.config.js
+++ apps/demo/webpack.prod.config.js
@@ -1,1 +1,9 @@
-module.exports = require("./webpack.config");
+const sharedConfig = require("./webpack.config");
+const webpackHelper = require("./webpackHelper");
+
+module.exports = {
+    ...sharedConfig,
+    plugins: webpackHelper.createPlugins({
+        portfolio: "https://code-star.github.io/nx-reference-portfolio",
+    }),
+};
